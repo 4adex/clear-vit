46f7ed22f3e930f3650ac68663469b1ebe3fe1d3
Rishav <ashu.rishav.k77@gmail.com>
2025-11-25 22:02:54 +0530

    Add RoPEAttention and ALiBiAttention to exports

--- models/blocks/__init__.py
+++ models/blocks/__init__.py
@@ -1,4 +1,7 @@
 from .mlp import MLPBlock
 from .rope_2d import (init_random_2d_freqs, compute_mixed_cis, init_t_xy, reshape_for_broadcast, apply_rotary_emb)
+from .rope_attention import RoPEAttention
+from .alibi_attention import ALiBiAttention
 
-__all__ = ['MLPBlock', 'init_random_2d_freqs', 'compute_mixed_cis', 'init_t_xy', 'reshape_for_broadcast', 'apply_rotary_emb']
+__all__ = ['MLPBlock', 'init_random_2d_freqs', 'compute_mixed_cis', 'init_t_xy', 'reshape_for_broadcast', 'apply_rotary_emb'
+           ,'RoPEAttention','ALiBiAttention']
