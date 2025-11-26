93da4df6d29ef0ca852caff517d305fcd53cb16a
rishavk7-bit <ashu.rishav.k77@gmail.com>
2025-11-26 23:29:18 +0530

    Update blocks __init__.py to export CPE modules

--- models/blocks/__init__.py
+++ models/blocks/__init__.py
@@ -2,6 +2,12 @@ from .mlp import MLPBlock
 from .rope_2d import (init_random_2d_freqs, compute_mixed_cis, init_t_xy, reshape_for_broadcast, apply_rotary_emb)
 from .rope_attention import RoPEAttention
 from .alibi_attention import ALiBiAttention
+from .cpe import (
+    ConditionalPositionEncoding,
+    CPE2D,
+    CPEBlock
+)
+
 
 __all__ = ['MLPBlock', 'init_random_2d_freqs', 'compute_mixed_cis', 'init_t_xy', 'reshape_for_broadcast', 'apply_rotary_emb'
-           ,'RoPEAttention','ALiBiAttention']
+           ,'RoPEAttention','ALiBiAttention','ConditionalPositionEncoding','CPE2D','CPEBlock']
